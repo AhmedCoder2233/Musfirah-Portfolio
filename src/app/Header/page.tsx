a0e757845399b6bec867e741666bc15c29105602
Ahmed <ahmedmemon3344@gmail.com>
2025-04-05 12:23:23 +0500

    Musfirah-Port

--- src/app/Header/page.tsx
+++ src/app/Header/page.tsx
@@ -129,7 +129,7 @@ const Header = () => {
               {theme === "dark" ? (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-blue-500"
+                  className="h-6 w-6 text-cyan-400"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -144,7 +144,7 @@ const Header = () => {
               ) : (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-blue-500"
+                  className="h-6 w-6 text-cyan-400"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
